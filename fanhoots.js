434ee5e36ffd7e7da1db4776a66dea5fb1c80633
Krishna Sai Gullapalli <gullapalli.krish@gmail.com>
2016-08-01 18:35:38 +0530

    Update fanhoots.js

--- fanhoots.js
+++ fanhoots.js
@@ -3,11 +3,17 @@ $(document).ready(function(){
 		//$(this).css('border','none');
 		$(this).css('outline','none');
 	});
+	  $('#join').hover(function(){
+    $(this).css('font-size','2.2em');
+    },
+    function(){
+    $(this).css('font-size','2em');
+  });
 });
 $(document).on('click', 'a', function(event){
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 400);
+    }, 250);
 });
